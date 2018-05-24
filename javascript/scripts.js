d566a44eeae310655a916e1712913df0f1ea4ccd
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-24 18:56:09 +0200

    JS changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,20 +1,20 @@
 $(function(){
 
-   function Button(text) {
-   this.text = text || 'Hello';
+  function Button(text) {
+    this.text = text || 'Hello';
   }
 
-    Button.prototype.create = function() {
+  Button.prototype.create = function() {
     var self = this;
     this.$element = $('<button>');
     this.$element.text(this.text);
     this.$element.on( "click", function() {
-    alert(self.text);
-  });
-      
+      alert(self.text);
+    });
+    
     $('body').append(this.$element);
-    };
+  };
 
-    var btn1 = new Button('Hello');
-    btn1.create();
+  var btn1 = new Button('Hello');
+  btn1.create();
 });
